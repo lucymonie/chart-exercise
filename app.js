5db1233994aacd0eb4ca38edf57d950065c9a539
lucymonie <lucy.monie.hall@gmail.com>
2017-08-03 07:06:01 +0100

    Adjusts chart controller

---
+++ app.js
@@ -0,0 +1,25 @@
+google.charts.load('current', {'packages':['bar']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable(
+  [
+    ['Year', 'Sales', 'Expenses', 'Profit'],
+    ['2014', 1000, 400, 200],
+    ['2015', 1170, 460, 250],
+    ['2016', 660, 1120, 300],
+    ['2017', 1030, 540, 350]
+  ] //replace this with data param
+  );
+
+  var options = {
+    chart: {
+      title: 'Company Performance',
+      subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+    }
+  }; //replace this with options param
+
+  var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}
